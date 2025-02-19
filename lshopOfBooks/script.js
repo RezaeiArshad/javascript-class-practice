fcab42ea1059553ca1414a79a68fc943ba2646cd
mehdi rezaei arshad <mehdirezaeiarshad@gmail.com>
2025-02-19 23:39:13 +0330

    Add files via upload

---
+++ lshopOfBooks/script.js
@@ -0,0 +1,60 @@
+
+
+
+const books = [
+    {
+        name: "The let them theory",
+        author: "Mel Robins",
+        price: 20.00,
+    },
+    {
+        name: "Forgotten Home Apothecary",
+        author: "Dr. Nicole Apelian",
+        price: 37.00,
+    },
+    {
+        name: "I Wish Someone Had Told Me . . .",
+        author: "Dana perino",
+        price: 20.30,
+    },
+    {
+        name: "The Technological Republic",
+        author: "Alexander C.Karp",
+        price: 22.41,
+    },
+    {
+        name: "Sunrice on the Reaping",
+        author: "Suzane Collins",
+        price: 19.59,
+    },
+    {
+        name: "Atomic Habits",
+        author: "James Clear",
+        price: 2.00,
+    },
+    {
+        name: "Inner Excellence",
+        author: "Jim Murphy",
+        price: 15.15,
+    },
+    {
+        name: "The Housemaid",
+        author: "Freida McFadden",
+        price: 1.93,
+    },
+    {
+        name: "Onyx Storm",
+        author: "Rebbeca Yarros",
+        price: 15.70,
+    },
+    {
+        name: "The Crash",
+        author: "Freida McFaffen",
+        price: 11.27,
+    },
+    {
+        name: "1984: 75th Anniversary",
+        author: "George Orwell",
+        price: 1.74,
+    }
+]
