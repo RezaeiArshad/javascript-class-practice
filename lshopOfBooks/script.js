6a6a921748f7d4dc7d774b96065d2ec9ad5a0da1
mehdi rezaei arshad <mehdirezaeiarshad@gmail.com>
2025-02-20 14:49:23 +0330

    that's that done but as always I'll need more . . .

--- lshopOfBooks/script.js
+++ lshopOfBooks/script.js
@@ -1,5 +1,11 @@
-
-
+const bookShelves = document.getElementById("books-container")
+const addToCartBtns = document.getElementsByClassName("add-to-cart-btn");
+const cartDiv = document.getElementById("cart");
+const showCartBtn = document.getElementById("show-cart")
+const selectedBooks = document.getElementById("selected-books")
+const clearCartBtn = document.getElementById("clear-cart")
+const numberOfSelectedBooks = document.getElementById("number-of-selected-books");
+const netAmount = document.getElementById("total-price");
 
 const books = [
     {
@@ -13,7 +19,7 @@ const books = [
         price: 37.00,
     },
     {
-        name: "I Wish Someone Had Told Me . . .",
+        name: "I Wish Someone Had Told Me",
         author: "Dana perino",
         price: 20.30,
     },
@@ -57,4 +63,94 @@ const books = [
         author: "George Orwell",
         price: 1.74,
     }
-]
+];
+
+books.forEach(({name, author, price}) => {
+    bookShelves.innerHTML += `
+    <div class="book" id="${name}-${author}">
+      <p><strong>${name}</strong></p>
+      <p>${author}</p>
+      <p>${price} $</p>
+      <button class="add-to-cart-btn">Add To Cart</button>
+    </div>
+    `
+});
+
+[...addToCartBtns].forEach((btn) => {
+    btn.addEventListener("click", (event) => {
+        cart.addItem(String(event.target.parentElement.id), books);
+    })
+})
+
+class ShoppingCart {
+    constructor () {
+    this.items = [];
+    this.numberOfItems = 0;
+    this.totalAmount = 0;
+    }
+
+    updateNumber(num) {
+        numberOfSelectedBooks.innerText = num;
+    }
+    
+    addItem(idOfTheCallingDiv, allBooks) {
+      const addedBook = allBooks.find((item) => `${item.name}-${item.author}` === idOfTheCallingDiv);
+      const {name, author, price} = addedBook;
+      selectedBooks.innerHTML += `
+      <div id="selected-book">
+        <p>${name}</p>
+        <p>price: <span>${price}</span> $</p>
+      </div>
+      `;
+      this.items.push(`${name}-${author}`);
+      this.numberOfItems = this.items.length;
+      this.updateNumber(this.numberOfItems);
+      this.addThePrices(price);
+    }
+
+    addThePrices(pri) {
+      const totalPrice = this.totalAmount += pri;
+      this.netAmount(totalPrice);
+    }
+
+    netAmount(totalPri) {
+      const tax = Math.ceil((totalPri / 100) * 9);
+      netAmount.innerText = tax + this.totalAmount;
+    }
+
+    clearCart() {
+      if (!this.numberOfItems) {
+        alert("Your cart is already empty");
+        return
+      }
+      else {
+      const youSure = confirm("Are you sure you want to clear the cart?");
+        if (youSure) {
+            this.items = [];
+            this.numberOfItems = 0;
+            this.totalAmount = 0;
+            selectedBooks.innerHTML = "";
+            netAmount.innerHTML = "";
+            numberOfSelectedBooks.innerHTML = "";
+        }
+        }
+    }
+}
+
+const cart = new ShoppingCart();
+
+clearCartBtn.addEventListener("click", cart.clearCart.bind(cart));
+
+const hideCart = () => {
+   cartDiv.style.display = "none";
+   showCartBtn.innerText = "show cart";
+   showCartBtn.onclick = showCart
+};
+
+const showCart = () => {
+  cartDiv.style.display = "block";
+  showCartBtn.innerText = "hide cart";
+  showCartBtn.onclick = hideCart;
+};
+
+showCartBtn.onclick = showCart;
